247f731d146e0a9b84702d97ef92588bad227fe8
老小杨 <tt17@tt17.com>
2023-05-26 06:30:54 +0800

    2

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -1189,7 +1189,7 @@ SpellCastResult Unit::CastSpell(SpellCastTargets const& targets, SpellInfo const
     std::string msg_1 = Acore::StringFormatFmt("Unit::CastSpell-7. name: {} guid:{} targets: {} spellInfo: {} value: {} triggerFlags: {} castItem: {} triggeredByAura: {} originalCaster: {}",
         GetName(),
         GetGUID().ToString(),
-        targets.ToString(),
+        targets.GetObjectTargetGUID().ToString(),
         "spellInfo",
         "value",
         "triggerFlags",
