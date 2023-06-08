dda25fb9c09d8188b3de409dfbe8aa21250b4027
老小杨 <tt17@tt17.com>
2023-06-08 22:57:30 +0800

    12313

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -1189,8 +1189,9 @@ SpellCastResult Unit::CastSpell(SpellCastTargets const& targets, SpellInfo const
 {
     //myLog 
 
-    std::string msg_0 = Acore::StringFormatFmt("<Unit.CastSpell_7_spell_{} target='{}' >"
+    std::string msg_0 = Acore::StringFormatFmt("<Unit.CastSpell_7_spell_{} unitId='{}' targetId='{}' target='{}' >"
         ,spellInfo ? spellInfo->Id : 0
+        ,GetGUID()->GetRawValue()
         ,targets.GetUnitTarget() ? targets.GetUnitTarget()->GetName() : "no"//todo 这里该放啥
     );
     Say(msg_0,LANG_UNIVERSAL);
